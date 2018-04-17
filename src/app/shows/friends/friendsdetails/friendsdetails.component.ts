b22705d3c183753081a08885a5678e78c4f990e7
briankhines@gmail.com <briankhines@gmail.com>
2018-04-16 20:58:33 -0400

    Added details page, notes entry

--- src/app/shows/friends/friendsdetails/friendsdetails.component.ts
+++ src/app/shows/friends/friendsdetails/friendsdetails.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ShareddataService } from '../../../shared/services/shareddata.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-friendsdetails',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./friendsdetails.component.css']
 })
 export class FriendsdetailsComponent implements OnInit {
+  episodeId: string;
+  episode: any;
+  noteEntry: string;
 
-  constructor() { }
+  constructor(private sharedData: ShareddataService, private route: ActivatedRoute, private _loc: Location) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.episodeId = params['id'];
+      this.episode = this.sharedData.getEpisodeById(this.episodeId);
+    });
   }
 
+  goBack() {
+    this._loc.back();
+  }
+
+  addNote() {
+    this.sharedData.addNote(this.episodeId, this.noteEntry);
+    this.clearEntry();
+  }
+
+  deleteNote(noteText: string) {
+    this.sharedData.deleteNote(this.episodeId, noteText);
+  }
+
+  clearEntry() {
+    this.noteEntry = '';
+  }
 }
--- src/app/shows/livepd/livepddetails/livepddetails.component.ts
+++ src/app/shows/livepd/livepddetails/livepddetails.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ShareddataService } from '../../../shared/services/shareddata.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-livepddetails',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./livepddetails.component.css']
 })
 export class LivepddetailsComponent implements OnInit {
+  episodeId: string;
+  episode: any;
+  noteEntry: string;
 
-  constructor() { }
+  constructor(private sharedData: ShareddataService, private route: ActivatedRoute, private _loc: Location) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.episodeId = params['id'];
+      this.episode = this.sharedData.getEpisodeById(this.episodeId);
+    });
   }
 
+  goBack() {
+    this._loc.back();
+  }
+
+  addNote() {
+    this.sharedData.addNote(this.episodeId, this.noteEntry);
+    this.clearEntry();
+  }
+
+  deleteNote(noteText: string) {
+    this.sharedData.deleteNote(this.episodeId, noteText);
+  }
+
+  clearEntry() {
+    this.noteEntry = '';
+  }
 }
